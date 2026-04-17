0446ae76db1fe36b06ae030dc8221ea48ff2d37f
Daniyal <thedaniiyal@gmail.com>
2026-04-17 17:36:33 +0530

    Added home and favorites page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
 import './App.css'
-import MovieCard from "./components/MovieCard.jsx"
+import Home from "./pages/Home"
 
 function App() {
   return (
     <>
-      <MovieCard movie={{title: "Tims Film", release_date: "2024",url: "https://image.tmdb.org/t/p/w500/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg"}}/>
-      <MovieCard movie={{title: "Cristiano GOAT",url:"https://imgcdn.stablediffusionweb.com/2024/11/21/4af75f36-1e82-4b07-b901-5f5fa22493bb.jpg"}}/>
+      <Home />
     </>
   )
 }
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import MovieCard from "../components/MovieCard"
+import {useState} from "react"
+
+function Home() {
+    const[searchQuery, setSearchQuery] = useState("");
+
+    const movies = [
+        { id: 1, title: "John Wick", release_date: "2020" },
+        { id: 2, title: "Terminatior", release_date: "1999" },
+        { id: 3, title: "The Matrix", release_date: "1998" },
+    ];
+
+    const handleSearch = () => {
+        alert(searchQuery)
+    }
+
+    return (
+        <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input type="text"
+                placeholder="Search for movies..."
+                className="search-input" 
+                value = {searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                />
+                <button type="submit" className="search-button">Search</button>
+            </form>
+
+            <div className="movies-grid">
+                {movies.map(movie => (
+                    <MovieCard movie={movie} key={movie.id} />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Home 
