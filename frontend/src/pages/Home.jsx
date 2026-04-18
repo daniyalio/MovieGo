abe76819c71ab9f2015f64deef6634787c59b5b1
Daniyal <thedaniiyal@gmail.com>
2026-04-18 20:01:31 +0530

    Added page routing and Navigation bar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,8 +10,10 @@ function Home() {
         { id: 3, title: "The Matrix", release_date: "1998" },
     ];
 
-    const handleSearch = () => {
-        alert(searchQuery)
+    const handleSearch = (e) => {
+        e.preventDefault();
+        alert(searchQuery);
+        setSearchQuery("");
     }
 
     return (
@@ -27,7 +29,8 @@ function Home() {
             </form>
 
             <div className="movies-grid">
-                {movies.map(movie => (
+                {movies.map(movie => 
+                movie.title.toLowerCase().startsWith(searchQuery) && (
                     <MovieCard movie={movie} key={movie.id} />
                 ))}
             </div>
